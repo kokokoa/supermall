fc4a765831f21017969e9e597ed225c3c78691e2
itcast <itheima@itcast.cn>
2021-09-21 22:33:18 +0800

    项目

--- src/components/common/tabbar/TabBar.vue
+++ src/components/common/tabbar/TabBar.vue
@@ -16,12 +16,13 @@ export default {
 #tab-bar {
     display: flex;
     position: fixed;
-    /* background: pink; */
+    background: #fff;
     bottom: 0px;
     /* top: 0px; */
     left: 0px;
     right: 0;
     box-shadow: 0px -1px 1px rgba(100, 100, 100, .2);
+    
     }
      
  
